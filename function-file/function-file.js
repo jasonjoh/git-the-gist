de6387c4f8dce8cf9840c2284ce8ba877f9b8c27
Jason Johnston <jasonjoh@microsoft.com>
2017-04-26 08:37:47 -0400

    Initial check in
    Addin created with generator beta.

---
+++ function-file/function-file.js
@@ -0,0 +1,71 @@
+var config;
+
+Office.initialize = function () {
+  config = getConfig();
+};
+
+function showError(error) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync('github-error', {
+    type: 'errorMessage',
+    message: error
+  }, function(result){
+  });
+}
+
+var settingsDialog;
+
+function insertDefaultGist(event) {
+
+  // Check if the add-in has been configured
+  if (config && config.defaultGistId) {
+    // Get the default Gist content and insert
+    try {
+      getGist(config.defaultGistId, function(gist, error) {
+        if (gist) {
+          buildBodyContent(gist, function (content, error) {
+            if (content) {
+              Office.context.mailbox.item.body.setSelectedDataAsync(content,
+                {coercionType: Office.CoercionType.Html}, function(result) {
+                  event.completed();
+              });
+            } else {
+              showError(error);
+              event.completed();
+            } 
+          });
+        } else {
+          showError(error);
+          event.completed();
+        }
+      });
+    } catch (err) {
+      showError(err);
+      event.completed();
+    }
+    
+  } else {
+    // Not configured yet, display settings dialog with
+    // warn=1 to display warning.
+    var url = new URI('../settings/dialog.html?warn=1').absoluteTo(window.location).toString();
+    var dialogOptions = { width: 20, height: 40 };
+    
+    Office.context.ui.displayDialogAsync(url, dialogOptions, function(result) {
+      settingsDialog = result.value;
+      settingsDialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, receiveMessage);
+      settingsDialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogEventReceived, dialogClosed);
+      event.completed();
+    });
+  }
+}
+
+function receiveMessage(message) {
+  config = JSON.parse(message.message);
+  setConfig(config, function(result) {
+    settingsDialog.close();
+    settingsDialog = null;
+  });
+}
+
+function dialogClosed(message) {
+  settingsDialog = null;
+}
